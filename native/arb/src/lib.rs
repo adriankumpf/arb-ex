0c1465a698f12450bce2a9f3dabc1143330caafe
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2021-03-19 17:47:54 +0100

    Return standard Result type

--- native/arb/src/lib.rs
+++ native/arb/src/lib.rs
@@ -1,62 +1,56 @@
 use arb;
 
-use rustler::types::atom;
-use rustler::{Encoder, Env, NifResult, NifStruct, Term};
+use rustler::{Atom, NifStruct, NifTuple, NifUntaggedEnum};
 
-mod reason {
-    rustler::atoms! {
-        not_found,
-        multiple_found,
-        verification_failed,
-        unsafe_read,
-        bad_device,
-        usb,
-        io,
+#[derive(NifTuple, Debug)]
+pub struct ErrorTuple(Atom, String);
+
+#[derive(NifUntaggedEnum, Debug)]
+enum ArbError {
+    Plain(Atom),
+    Extended(ErrorTuple),
+}
+
+impl From<arb::Error> for ArbError {
+    fn from(err: arb::Error) -> Self {
+        mod reason {
+            rustler::atoms! { not_found, multiple_found, verification_failed, unsafe_read, bad_device, usb, io, }
+        }
+
+        match err {
+            arb::Error::NotFound => ArbError::Plain(reason::not_found()),
+            arb::Error::MultipleFound => ArbError::Plain(reason::multiple_found()),
+            arb::Error::VerificationFailed => ArbError::Plain(reason::verification_failed()),
+            arb::Error::UnsafeRead => ArbError::Plain(reason::unsafe_read()),
+            arb::Error::BadDevice => ArbError::Plain(reason::bad_device()),
+            arb::Error::Usb(e) => ArbError::Extended(ErrorTuple(reason::usb(), format!("{}", e))),
+            arb::Error::IO(e) => ArbError::Extended(ErrorTuple(reason::io(), format!("{}", e))),
+        }
     }
 }
 
 #[derive(NifStruct)]
 #[module = "Arb.Native.Options"]
 struct Options {
-    pub port: Option<u8>,
-    pub verify: bool,
-}
-
-fn arb_error_to_term<'a>(env: Env<'a>, err: arb::Error) -> Term<'a> {
-    let reason = match err {
-        arb::Error::NotFound => reason::not_found().encode(env),
-        arb::Error::MultipleFound => reason::multiple_found().encode(env),
-        arb::Error::VerificationFailed => reason::verification_failed().encode(env),
-        arb::Error::UnsafeRead => reason::unsafe_read().encode(env),
-        arb::Error::BadDevice => reason::bad_device().encode(env),
-        arb::Error::Usb(reason) => (reason::usb(), format!("{}", reason).encode(env)).encode(env),
-        arb::Error::IO(reason) => (reason::io(), format!("{}", reason).encode(env)).encode(env),
-    };
-
-    (atom::error(), reason).encode(env)
+    port: Option<u8>,
+    verify: bool,
 }
 
 #[rustler::nif(schedule = "DirtyCpu")]
-fn activate<'a>(env: Env<'a>, relays: Vec<u8>, options: Options) -> NifResult<Term<'a>> {
-    let relays: u8 = relays
+fn activate(relays: Vec<u8>, options: Options) -> Result<(), ArbError> {
+    let relays = relays
         .into_iter()
         .filter(|&r| r != 0)
         .fold(0, |acc, r| acc | 1 << (r - 1));
 
-    match arb::set_status(relays, options.verify, options.port) {
-        Err(err) => Ok(arb_error_to_term(env, err)),
-        Ok(()) => Ok(atom::ok().encode(env)),
-    }
+    Ok(arb::set_status(relays, options.verify, options.port)?)
 }
 
 #[rustler::nif(schedule = "DirtyCpu")]
-fn get_active<'a>(env: Env<'a>, port: Option<u8>) -> NifResult<Term<'a>> {
-    let result = match arb::get_status(port) {
-        Err(err) => return Ok(arb_error_to_term(env, err)),
-        Ok(inner) => inner,
-    };
+fn get_active(port: Option<u8>) -> Result<Vec<u8>, ArbError> {
+    let result = arb::get_status(port)?;
 
-    let active_relays: Vec<u8> = (0..8)
+    let active_relays = (0..8)
         .filter_map(|m| {
             if (1 << m) & result != 0 {
                 Some(m + 1)
@@ -66,15 +60,12 @@ fn get_active<'a>(env: Env<'a>, port: Option<u8>) -> NifResult<Term<'a>> {
         })
         .collect();
 
-    Ok((atom::ok(), active_relays).encode(env))
+    Ok(active_relays)
 }
 
 #[rustler::nif(schedule = "DirtyCpu")]
-fn reset<'a>(env: Env<'a>, port: Option<u8>) -> NifResult<Term<'a>> {
-    match arb::reset(port) {
-        Err(err) => return Ok(arb_error_to_term(env, err)),
-        Ok(()) => Ok(atom::ok().encode(env)),
-    }
+fn reset(port: Option<u8>) -> Result<(), ArbError> {
+    Ok(arb::reset(port)?)
 }
 
 rustler::init!("Elixir.Arb.Native", [activate, get_active, reset]);
